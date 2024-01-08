146a6cc75d6d9f08e86ed509b432f94b72c08b95
anisul islam <anisulislam980340@gmail.com>
2024-01-08 17:02:21 +0600

    service and contact us model completed

--- contact_us/admin.py
+++ contact_us/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import ContactUs
 # Register your models here.
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display=["name","phone_no","problem"]
+    
+admin.site.register(ContactUs,ContactUsAdmin)
