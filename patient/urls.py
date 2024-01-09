373aa889eae0aebf450346348df88cec35cb1548
anisul islam <anisulislam980340@gmail.com>
2024-01-10 00:18:18 +0600

    patient api completed

---
+++ patient/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from . import views
+from django.urls import path,include
+
+router=DefaultRouter()
+router.register('list',views.PatientViewSets)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from . import models
+from . import serializers
 # Create your views here.
+
+class PatientViewSets(viewsets.ModelViewSet):
+    queryset=models.Patient.objects.all()
+    serializer_class=serializers.PatientSerializer
