60e0bbadb8f3a34a7f04577b451fed9e0053fce1
anisul islam <anisulislam980340@gmail.com>
2024-01-10 11:00:59 +0600

    custom filter implement

--- patient/views.py
+++ patient/views.py
@@ -7,3 +7,10 @@ from . import serializers
 class PatientViewSets(viewsets.ModelViewSet):
     queryset=models.Patient.objects.all()
     serializer_class=serializers.PatientSerializer
+    
+    def get_queryset(self):
+        queryset=super().get_queryset()
+        patient_id=self.request.query_params.get('patient_id')
+        if patient_id is not None:
+            queryset=queryset.filter(patient_id=patient_id)
+        return queryset
