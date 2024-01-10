da19c1fbfed6c6b087fcc7698109a07397a92073
anisul islam <anisulislam980340@gmail.com>
2024-01-10 10:57:42 +0600

    appointment api completed

---
+++ appointment/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from . import models
+
+class AppointmentSerializer(serializers.ModelSerializer):
+   
+   
+    class Meta:
+        model = models.Appointment
+        fields = '__all__'
